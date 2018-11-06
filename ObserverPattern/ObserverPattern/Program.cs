182331ef2925d19e6eed323aa511b829cf546257
marlonssantos <marlonssantos@users.noreply.github.com>
2018-11-06 17:01:19 -0600

    Add files via upload

---
+++ ObserverPattern/ObserverPattern/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObserverPattern
+{
+    public class MainApp
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        static void Main()
+        {
+            // Create IBM stock and attach investors
+
+            IBM ibm = new IBM("IBM", 120.00);
+            ibm.Attach(new Investor("Marlon Santos"));
+            ibm.Attach(new Investor("John Smith"));
+
+            Dell dell = new Dell("Dell", 200.00);
+            dell.Attach(new Investor("John Smith"));
+            dell.Attach(new Investor("Marlon Santos"));
+            
+            // Fluctuating prices will notify investors
+            ibm.Price = 120.10;
+            ibm.Price = 121.00;
+            ibm.Price = 120.50;
+            ibm.Price = 120.75;
+
+            dell.Price = 201.00;
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Subject' abstract class
+    /// </summary>
+    abstract class Stock
+    {
+        private readonly string _symbol;
+        private double _price;
+        private readonly List<IInvestor> _investors = new List<IInvestor>();
+
+        // Constructor
+
+        public Stock(string symbol, double price)
+        {
+            this._symbol = symbol;
+            this._price = price;
+        }
+
+        /// <summary>
+        /// Attaches an investor to a specific stock symbol
+        /// </summary>
+        /// <param name="investor"></param>
+        public void Attach(IInvestor investor)
+        {
+            _investors.Add(investor);
+        }
+
+        public void Detach(IInvestor investor)
+        {
+            _investors.Remove(investor);
+        }
+
+        public void Notify()
+        {
+            foreach (IInvestor investor in _investors)
+            {
+                investor.Update(this);
+            }
+
+            Console.WriteLine("");
+        }
+
+        // Gets or sets the price
+        public double Price
+        {
+            get { return _price; }
+            set
+            {
+                if (_price != value)
+                {
+                    _price = value;
+                    Notify();
+                }
+            }
+        }
+
+        // Gets the symbol
+        public string Symbol
+        {
+            get { return _symbol; }
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteSubject' class
+    /// </summary>
+    internal class IBM : Stock
+    {
+        // Constructor
+        public IBM(string symbol, double price) : base(symbol, price)
+        {
+        }
+    }
+
+    internal class Dell : Stock
+    {
+        public Dell(string symbol, double price): base(symbol, price)
+        {
+        }
+    }
+
+    /// <summary>
+    /// The 'Observer' interface
+    /// </summary>
+    internal interface IInvestor
+    {
+        void Update(Stock stock);
+    }
+
+    /// <summary>
+    /// The 'ConcreteObserver' class
+    /// </summary>
+    internal class Investor : IInvestor
+    {
+        private readonly string _name;
+        private Stock _stock;
+
+        // Constructor
+        public Investor(string name)
+        {
+            this._name = name;
+        }
+
+        public void Update(Stock stock)
+        {
+            Console.WriteLine($"Notified {_name} of {stock.Symbol}'s change to {stock.Price:C}");
+        }
+
+        // Gets or sets the stock
+        public Stock Stock
+        {
+            get { return _stock; }
+            set { _stock = value; }
+        }
+    }
+}
+
